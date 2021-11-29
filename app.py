a8b4901241f633629c0019f84f606d801dec5d17
marius stoian <mariusiosefstoian@gmail.com>
2021-11-29 15:16:12 +0000

    first push, uniting dirs

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+'''
+# BTC volume correlation with public economy sentiment
+'''
+
+url = 'https://cloudsentiment-gijujv7fiq-ew.a.run.app/'
+
+if url == 'https://cloudsentiment-gijujv7fiq-ew.a.run.app/':
+    st.markdown(
+        'Test api prediction'
+    )
+
+st.markdown('Test.')
